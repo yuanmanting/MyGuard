ef40b0adf6ae5a4291f1473f54f0b0d41f32639a
yuanmanting <842539314@qq.com>
2017-09-24 19:35:50 +0800

    handler处理

--- app/src/main/java/com/me/android/myguard/m1home/utils/VersionUpdateUtils.java
+++ app/src/main/java/com/me/android/myguard/m1home/utils/VersionUpdateUtils.java
@@ -1,7 +1,15 @@
 package com.me.android.myguard.m1home.utils;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
 
+import com.me.android.myguard.R;
+import com.me.android.myguard.m1home.HomeActivity;
 import com.me.android.myguard.m1home.entity.VersionEntity;
 
 import org.apache.http.HttpEntity;
@@ -21,9 +29,36 @@ import java.io.IOException;
  */
 
 public class VersionUpdateUtils {
-  private String mVersion;
+    private String mVersion;
     private Activity context;
     private VersionEntity versionEntity;
+    private static final int MESSAGE_IO_ERROR=102;
+    private static final int MESSAGE_JSON_ERROR=103;
+    private static final int MESSAGE_SHOW_DIALOG=104;
+    private static final int MESSAGE_ENTERHOME=105;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MESSAGE_IO_ERROR:
+                    Toast.makeText(context,"IO错误",Toast.LENGTH_LONG).show();
+                    break;
+                case MESSAGE_JSON_ERROR:
+                    Toast.makeText(context,"JSON解析错误",Toast.LENGTH_LONG).show();
+                    break;
+                case MESSAGE_SHOW_DIALOG:
+                    showUpdateDialog(versionEntity);
+                    break;
+                case MESSAGE_ENTERHOME:
+                    Intent intent=new Intent(context, HomeActivity.class);
+                    context.startActivity(intent);
+                    context.finish();
+                    break;
+            }
+        }
+    };
+
+
     public VersionUpdateUtils(String mVersion,Activity context){
         this.mVersion=mVersion;
         this.context=context;
@@ -48,19 +83,51 @@ public class VersionUpdateUtils {
                versionEntity.description = jsonObject.getString("des");
                versionEntity.apkurl = jsonObject.getString("apkurl");
                if (!mVersion.equals(versionEntity.versioncode)) {
-                   System.out.println(versionEntity.description);
-                   DownloadsUtils downloadsUtils = new DownloadsUtils();
-                   downloadsUtils.downloadApk(versionEntity.apkurl, "myguard.apk", context);
+//                   System.out.println(versionEntity.description);
+//                   DownloadsUtils downloadsUtils = new DownloadsUtils();
+//                   downloadsUtils.downloadApk(versionEntity.apkurl, "myguard.apk", context);
+                   handler.sendEmptyMessage(MESSAGE_SHOW_DIALOG);
                }
            }
        }catch (IOException e) {
+           handler.sendEmptyMessage(MESSAGE_IO_ERROR);
            e.printStackTrace();
        }catch (JSONException e){
            e.printStackTrace();
+          handler.sendEmptyMessage(MESSAGE_JSON_ERROR);
             }
 
 
         }
+        private void  showUpdateDialog(final VersionEntity versionEntity){
+            AlertDialog.Builder builder=new AlertDialog.Builder(context);
+            builder.setTitle("检查到有新版本："+versionEntity.versioncode);
+            builder.setMessage(versionEntity.description);
+            builder.setCancelable(false);
+            builder.setIcon(R.mipmap.ic_launcher_round);
+            builder.setPositiveButton("立刻升级", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                   downloadNewApk(versionEntity.apkurl);
+                }
+            });
+            builder.setNegativeButton("暂不升级", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                    enterHome();
+                }
+            });
+            builder.show();
+        }
+        private void  enterHome(){
+            handler.sendEmptyMessage(MESSAGE_ENTERHOME);
+        }
+        private void downloadNewApk(String apkurl){
+            DownloadsUtils downloadsUtils=new DownloadsUtils();
+            downloadsUtils.downloadApk(apkurl,"myguard.apk",context);
+        }
+
 
     }
 
