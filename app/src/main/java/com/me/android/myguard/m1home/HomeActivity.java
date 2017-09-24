2fb51cbd3393699fd21dfcc129369b92c055ee7b
linhuiyin <11969938509@qq.com>
2017-09-24 22:19:57 +0800

    完成主界面

--- app/src/main/java/com/me/android/myguard/m1home/HomeActivity.java
+++ app/src/main/java/com/me/android/myguard/m1home/HomeActivity.java
@@ -1,15 +1,49 @@
 package com.me.android.myguard.m1home;
 
+import android.os.Build;
+import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import com.me.android.myguard.R;
+import com.me.android.myguard.m1home.adapter.HomeAdapter;
 
 public class HomeActivity extends AppCompatActivity {
-
+    private GridView gv_home;
+    private long mExitTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
+        getSupportActionBar().hide();
+        gv_home=(GridView)findViewById(R.id.gv_home);
+        gv_home.setAdapter(new HomeAdapter(HomeActivity.this));
+        gv_home.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+                switch (i){
+
+                }
+            }
+        });
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK){
+            if((SystemClock.currentThreadTimeMillis()-mExitTime)<2000){
+                System.exit(0);
+            }else{
+                Toast.makeText(this,"再按一次退出程序",Toast.LENGTH_LONG).show();
+                mExitTime= System.currentTimeMillis();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
