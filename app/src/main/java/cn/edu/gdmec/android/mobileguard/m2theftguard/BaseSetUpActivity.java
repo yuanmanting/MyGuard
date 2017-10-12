a10a50dc529040e31d295368104ed760ab6619b0
linhuiyin <11969938509@qq.com>
2017-10-12 13:11:21 +0800

    文件创建好了

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/BaseSetUpActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+public class BaseSetUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_set_up);
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup1Activity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+
+public class Setup1Activity extends BaseSetUpActivity {
+}
