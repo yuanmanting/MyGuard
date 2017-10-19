a8689e1cd84d2df9b79d990215f3688218c8e8dc
linhuiyin <11969938509@qq.com>
2017-10-19 23:02:31 +0800

    做到第八那里，实现了SIM卡绑定

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/App.java
@@ -0,0 +1,58 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+
+public class App extends Application {
+    /**
+     * Called when the application is starting, before any activity, service,
+     * or receiver objects (excluding content providers) have been created.
+     * Implementations should be as quick as possible (for example using
+     * lazy initialization of state) since the time spent in this function
+     * directly impacts the performance of starting the first activity,
+     * service, or receiver in a process.
+     * If you override this method, be sure to call super.onCreate().
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        correctSIM();
+    }
+    public void correctSIM(){
+        //检查sim卡是否发生变化
+        SharedPreferences sp=getSharedPreferences("config", Context.MODE_PRIVATE);
+        //获取防盗保护的状态
+        boolean protecting=sp.getBoolean("protecting",true);
+        if(protecting){
+            //得到绑定的sim卡串号
+            String bindsim=sp.getString("sim","");
+            //得到手机现在的sim卡串号
+            TelephonyManager tm=(TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+            //为了测试在手机序列号上data以模拟SIM卡被更换的情况
+            String realsim=tm.getSimSerialNumber();
+            //因为虚拟机无法更换sim卡，所以虚拟机测试要有此代码，真机测试要注释这段代码
+            realsim="999";
+            if(bindsim.equals(realsim)){
+                Log.i("","sim卡未发生变化，还是您的手机");
+
+            }else{
+                Log.i("","SIM卡变化了");
+                //由于系统版本的原因，这里的发短信可能与其他手机版本不兼容
+                String safenumber=sp.getString("safephone","");
+                if(!TextUtils.isEmpty(safenumber)){
+                    SmsManager smsManager=SmsManager.getDefault();
+                    smsManager.sendTextMessage(safenumber,null,"你的亲友手机的SIM卡已经被更换！",null,null);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/Setup3Activity.java
@@ -1,7 +1,13 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
@@ -9,21 +15,58 @@ import cn.edu.gdmec.android.mobileguard.R;
  * Created by Administrator on 2017/10/12.
  */
 
-public class Setup3Activity extends BaseSetUpActivity {
+public class Setup3Activity extends BaseSetUpActivity implements View.OnClickListener{
+    private EditText mInputPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_3);
         ((RadioButton) findViewById(R.id.rb_third)).setChecked(true);
+        findViewById(R.id.btn_addcontact).setOnClickListener(this);
+        mInputPhone=(EditText)findViewById(R.id.et_inputphone);
+        String safephone=sp.getString("safephone",null);
+        if(!TextUtils.isEmpty(safephone)){
+            mInputPhone.setText(safephone);
+        }
     }
 
     @Override
     public void showNext() {
+        //判断文本输入框中是否有电话号码
+        String safephone=mInputPhone.getText().toString().trim();
+        if(TextUtils.isEmpty(safephone)){
+            Toast.makeText(this,"请输入安全号码",Toast.LENGTH_LONG).show();
+            return;
+        }
+        SharedPreferences.Editor edit=sp.edit();
+        edit.putString("safephone",safephone);
+        edit.commit();
         startActivityAndFinishSelf(Setup4Activity.class);
     }
 
     @Override
     public void showPre() {
-startActivityAndFinishSelf(Setup2Activity.class);
+        startActivityAndFinishSelf(Setup2Activity.class);
     }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_addcontact:
+                //启动联系人选择activity并获取返回值
+                startActivityForResult(new Intent(this,ContactSelectActivity.class),0);
+                break;
+        }
+    }
+    //获取被调用的activity的返回值
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(data!=null){
+            String phone=data.getStringExtra("phone");
+            mInputPhone.setText(phone);
+        }
+    }
+
 }
