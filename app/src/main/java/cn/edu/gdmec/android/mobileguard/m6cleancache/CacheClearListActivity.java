bc0c851045852b369bc0979c4d6374785ef17e09
sun1969938509 <1969938509@qq.com>
2017-11-24 14:37:09 +0800

    做到10.14章，public void getCacheSize(PackageInfo info)这里

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/CacheClearListActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/CacheClearListActivity.java
@@ -1,17 +1,134 @@
 package cn.edu.gdmec.android.mobileguard.m6cleancache;
 
+
+import android.content.pm.IPackageStatsObserver;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.Formatter;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.FormattableFlags;
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m6cleancache.adapter.CacheCleanAdapter;
+import cn.edu.gdmec.android.mobileguard.m6cleancache.entity.CacheInfo;
 
 /**
  * Created by Administrator on 2017/11/23.
  */
 
-public class CacheClearListActivity implements View.OnClickListener {
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
+public class CacheClearListActivity extends AppCompatActivity implements View.OnClickListener {
+   protected  static final int SCANNING=100;
+    protected static final int FINISH=10;
+    private AnimationDrawable animation;
+    private TextView mRecomandTV;
+    private TextView mCanCleanTV;
+    private long cacheMemory;
+    private List<CacheInfo> cacheInfos=new ArrayList<CacheInfo>();
+    private List<CacheInfo> mCacheInfos=new ArrayList<CacheInfo>();
+    private PackageManager pm;
+    private CacheCleanAdapter adapter;
+    private ListView mCacheLV;
+    private Button mCacheBtn;
+    private Thread thread;
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case SCANNING:
+                    PackageInfo info=(PackageInfo)msg.obj;
+                    mRecomandTV.setText("正在扫描:"+info.packageName);
+                    mCanCleanTV.setText("已扫描缓存:"+ Formatter.formatFileSize(CacheClearListActivity.this,cacheMemory));
+
+                    mCacheInfos.clear();
+                    mCacheInfos.addAll(cacheInfos);
+
+                    adapter.notifyDataSetChanged();
+                    mCacheLV.setSelection(mCacheInfos.size());
+                    break;
+                case FINISH:
+                    animation.stop();
+                    if(cacheMemory>0){
+                        mCacheBtn.setEnabled(true);
+
+                    }else{
+                        mCacheBtn.setEnabled(false);
+                        Toast.makeText(CacheClearListActivity.this,"您的手机洁净如新",Toast.LENGTH_LONG).show();
+                    }
+                    break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cache_clear_list);
+        pm=getPackageManager();
+        initView();
+    }
+    private void initView(){
+        findViewById(R.id.rl_titlebar).setBackgroundColor(getResources().getColor(R.color.rose_red));
+        ImageView mLeftImgv=(ImageView)findViewById(R.id.imgv_leftbtn);
+        mLeftImgv.setOnClickListener(this);
+        mLeftImgv.setImageResource(R.drawable.back);
+        ((TextView)findViewById(R.id.tv_title)).setText("缓存扫描");
+        mRecomandTV=(TextView)findViewById(R.id.tv_recommend_clean);
+        mCanCleanTV=(TextView)findViewById(R.id.tv_can_clean);
+        mCacheLV=(ListView)findViewById(R.id.lv_scancache);
+        mCacheBtn=(Button)findViewById(R.id.btn_cleanall);
+        mCacheBtn.setOnClickListener(this);
+        animation=(AnimationDrawable)findViewById(R.id.imgv_broom).getBackground();
+        animation.setOneShot(false);
+        animation.start();
+        adapter=new CacheCleanAdapter(this,mCacheInfos);
+        fillData();
+    }
+    private void fillData(){
+        thread=new Thread(){
+            public  void run(){
+                cacheInfos.clear();
+                List<PackageInfo> infos=pm.getInstalledPackages(0);
+                for (PackageInfo info:infos){
+                    getCacheSize(info);
+                    try{
+                        Thread.sleep(50);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                    Message msg=Message.obtain();
+                    msg.obj=info;
+                    msg.what=SCANNING;
+                    handler.sendMessage(msg);
+                }
+
+            }
+        };
+        thread.start();
+    }
+    public  void getCacheSize(PackageInfo info){
+        try {
+            Method method=PackageManager.class.getDeclaredMethod("getPackageSizeInfo",String.class, IPackageStatsObserver.class);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onClick(View v) {
 
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/adapter/CacheCleanAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/adapter/CacheCleanAdapter.java
@@ -1,68 +1,75 @@
 package cn.edu.gdmec.android.mobileguard.m6cleancache.adapter;
 
-import android.view.LayoutInflater;
+import android.content.Context;
+
+import android.text.format.Formatter;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+import java.util.List;
+import java.util.concurrent.LinkedTransferQueue;
+
+import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m6cleancache.entity.CacheInfo;
 
 /**
  * Created by Administrator on 2017/11/23.
  */
 
 public class CacheCleanAdapter extends BaseAdapter {
-    /**
-     * How many items are in the data set represented by this Adapter.
-     *
-     * @return Count of items.
-     */
+  private Context context;
+    private List<CacheInfo> cacheInfos;
+    public CacheCleanAdapter(Context context,List<CacheInfo> cacheInfos){
+        super();
+        this.context=context;
+        this.cacheInfos=cacheInfos;
+    }
     @Override
     public int getCount() {
-        return 0;
+        return cacheInfos.size();
     }
 
-    /**
-     * Get the data item associated with the specified position in the data set.
-     *
-     * @param position Position of the item whose data we want within the adapter's
-     *                 data set.
-     * @return The data at the specified position.
-     */
+
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int i) {
+        return cacheInfos.get(i);
     }
 
-    /**
-     * Get the row id associated with the specified position in the list.
-     *
-     * @param position The position of the item within the adapter's data set whose row id we want.
-     * @return The id of the item at the specified position.
-     */
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int i) {
+        return i;
     }
 
-    /**
-     * Get a View that displays the data at the specified position in the data set. You can either
-     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-     * parent View (GridView, ListView...) will apply default layout parameters unless you use
-     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
-     * to specify a root view and to prevent attachment to the root.
-     *
-     * @param position    The position of the item within the adapter's data set of the item whose view
-     *                    we want.
-     * @param convertView The old view to reuse, if possible. Note: You should check that this view
-     *                    is non-null and of an appropriate type before using. If it is not possible to convert
-     *                    this view to display the correct data, this method can create a new view.
-     *                    Heterogeneous lists can specify their number of view types, so that this View is
-     *                    always of the right type (see {@link #getViewTypeCount()} and
-     *                    {@link #getItemViewType(int)}).
-     * @param parent      The parent that this view will eventually be attached to
-     * @return A View corresponding to the data at the specified position.
-     */
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder=null;
+        if(view==null){
+            holder=new ViewHolder();
+            view=View.inflate(context, R.layout.item_cacheclean_list,null);
+            holder.mAppIconImgv=(ImageView)view.findViewById(R.id.imgv_appicon_cacheclean);
+            holder.mAppNameTV=(TextView)view.findViewById(R.id.tv_appname_cacheclean);
+            holder.mCacheSizeTV=(TextView)view.findViewById(R.id.tv_appsize_cacheclean);
+            view.setTag(holder);
+        }else {
+            holder=(ViewHolder)view.getTag();
+        }
+        CacheInfo cacheInfo=cacheInfos.get(i);
+        holder.mAppIconImgv.setImageDrawable(cacheInfo.appIcon);
+        holder.mAppNameTV.setText(cacheInfo.appName);
+        holder.mCacheSizeTV.setText(Formatter.formatFileSize(context,cacheInfo.cacheSize));
+        return view;
+    }
+    static class ViewHolder{
+        ImageView mAppIconImgv;
+        TextView mAppNameTV;
+        TextView mCacheSizeTV;
     }
 }
