850d78a09f00a01ade4c5cbf961548016a6c49ca
sun1969938509 <1969938509@qq.com>
2017-12-15 23:27:57 +0800

    敲到18.15

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/adapter/AppLockAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/adapter/AppLockAdapter.java
@@ -1,68 +1,78 @@
 package cn.edu.gdmec.android.mobileguard.m9advancedtools.adapter;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.R;
+import cn.edu.gdmec.android.mobileguard.m4appmanager.entity.AppInfo;
 
 /**
  * Created by Administrator on 2017/12/14.
  */
 
 public class AppLockAdapter extends BaseAdapter {
-    /**
-     * How many items are in the data set represented by this Adapter.
-     *
-     * @return Count of items.
-     */
+   private List<AppInfo> appInfos;
+    private Context context;
+    public AppLockAdapter(List<AppInfo> appInfos,Context context){
+        super();
+        this.appInfos=appInfos;
+        this.context=context;
+    }
     @Override
     public int getCount() {
-        return 0;
+        return appInfos.size();
     }
 
-    /**
-     * Get the data item associated with the specified position in the data set.
-     *
-     * @param position Position of the item whose data we want within the adapter's
-     *                 data set.
-     * @return The data at the specified position.
-     */
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Object getItem(int i) {
+        return appInfos.get(i);
     }
 
-    /**
-     * Get the row id associated with the specified position in the list.
-     *
-     * @param position The position of the item within the adapter's data set whose row id we want.
-     * @return The id of the item at the specified position.
-     */
+
     @Override
-    public long getItemId(int position) {
-        return 0;
+    public long getItemId(int i) {
+        return i;
     }
 
-    /**
-     * Get a View that displays the data at the specified position in the data set. You can either
-     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
-     * parent View (GridView, ListView...) will apply default layout parameters unless you use
-     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
-     * to specify a root view and to prevent attachment to the root.
-     *
-     * @param position    The position of the item within the adapter's data set of the item whose view
-     *                    we want.
-     * @param convertView The old view to reuse, if possible. Note: You should check that this view
-     *                    is non-null and of an appropriate type before using. If it is not possible to convert
-     *                    this view to display the correct data, this method can create a new view.
-     *                    Heterogeneous lists can specify their number of view types, so that this View is
-     *                    always of the right type (see {@link #getViewTypeCount()} and
-     *                    {@link #getItemViewType(int)}).
-     * @param parent      The parent that this view will eventually be attached to
-     * @return A View corresponding to the data at the specified position.
-     */
+
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(view!=null&&view instanceof RelativeLayout){
+            holder=(ViewHolder)view.getTag();
+
+        }else{
+            holder=new ViewHolder();
+            view=View.inflate(context, R.layout.item_list_applock,null);
+            holder.mAppIconImgv=(ImageView)view.findViewById(R.id.imgv_appicon);
+            holder.mAppNameTV=(TextView)view.findViewById(R.id.tv_appname);
+            holder.mLockIcon=(ImageView)view.findViewById(R.id.imgv_lock);
+            view.setTag(holder);
+        }
+        final AppInfo appInfo=appInfos.get(i);
+        holder.mAppIconImgv.setImageDrawable(appInfo.icon);
+        holder.mAppNameTV.setText(appInfo.appName);
+        if(appInfo.isLock){
+            holder.mLockIcon.setBackgroundResource(R.drawable.applock_icon);
+
+        }else{
+            holder.mLockIcon.setBackgroundResource(R.drawable.appunlock_icon);
+        }
+        return view;
+    }
+    static class ViewHolder{
+        TextView mAppNameTV;
+        ImageView mAppIconImgv;
+        ImageView mLockIcon;
+
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/service/AppLockService.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9advancedtools/service/AppLockService.java
@@ -1,39 +1,143 @@
 package cn.edu.gdmec.android.mobileguard.m9advancedtools.service;
 
+import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.App;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.EnterPswActivity;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.db.dao.AppLockDao;
 
 /**
  * Created by Administrator on 2017/12/14.
  */
 
 public class AppLockService extends Service {
-    /**
-     * Return the communication channel to the service.  May return null if
-     * clients can not bind to the service.  The returned
-     * {@link IBinder} is usually for a complex interface
-     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
-     * aidl</a>.
-     * <p>
-     * <p><em>Note that unlike other application components, calls on to the
-     * IBinder interface returned here may not happen on the main thread
-     * of the process</em>.  More information about the main thread can be found in
-     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
-     * Threads</a>.</p>
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     * @return Return an IBinder through which clients can call on to the
-     * service.
-     */
-    @Nullable
+  private boolean flag=false;
+    private AppLockDao dao;
+    private Uri uri=Uri.parse(App.APPLOCK_CONTENT_URI);
+    private List<String> packagenames;
+    private Intent intent;
+    private ActivityManager am;
+    private List<ActivityManager.RunningTaskInfo> taskInfos;
+    private ActivityManager.RunningTaskInfo taskInfo;
+    private String packagename;
+    private String tempStopProtectPackname;
+    private AppLockReceiver receiver;
+    private MyObserver observer;
+
+    class AppLockReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(App.APPLOCK_ACTION.equals(intent.getAction())){
+                tempStopProtectPackname=intent.getStringExtra("packagename");
+
+            }else if(Intent.ACTION_SCREEN_OFF.equals(intent.getAction())){
+                tempStopProtectPackname=null;
+                flag=false;
+            }else if(Intent.ACTION_SCREEN_ON.equals(intent.getAction())){
+                if(flag==true){
+                    startApplockService();
+                }
+            }
+        }
+    }
+    class MyObserver extends ContentObserver{
+        public MyObserver(Handler handler){
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            packagenames=dao.findAll();
+            super.onChange(selfChange);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        dao=new AppLockDao(this);
+        packagenames=dao.findAll();
+        if(packagenames.size()==0){
+            return;
+        }
+        observer=new MyObserver(new Handler());
+        getContentResolver().registerContentObserver(uri,true,observer);
+        receiver=new AppLockReceiver();
+        IntentFilter filter=new IntentFilter(App.APPLOCK_ACTION);
+        filter.addAction(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(receiver,filter);
+        intent=new Intent(AppLockService.this, EnterPswActivity.class);
+        am=(ActivityManager)getSystemService(ACTIVITY_SERVICE);
+        startApplockService();
+        super.onCreate();
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+    private void startApplockService(){
+        new Thread(){
+            public  void run(){
+                flag=true;
+                while (flag){
+                    if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+                        UsageStatsManager m=(UsageStatsManager)getSystemService(Context.USAGE_STATS_SERVICE);
+                        if(m!=null){
+                            long now=System.currentTimeMillis();
+                            List<UsageStats> stats=m.queryUsageStats(UsageStatsManager.INTERVAL_BEST,now-60*1000,now);
+                            String topActivity="";
+                            if((stats!=null)&&(!stats.isEmpty())){
+                                int j=0;
+                                for(int i=0;i<stats.size();i++){
+                                    if(stats.get(i).getLastTimeUsed()>stats.get(j).getLastTimeUsed()){
+                                        j=i;
+                                    }
+                                }
+                                packagename=stats.get(j).getPackageName();
+
+                            }
+                        }else{
+                            taskInfos=am.getRunningTasks(1);
+                            taskInfo=taskInfos.get(0);
+                            packagename=taskInfo.topActivity.getPackageName();
+
+                        }
+                        System.out.println(packagename);
+                        if(packagenames.contains(packagename)){
+                            if(!packagename.equals(tempStopProtectPackname)){
+                                intent.putExtra("packagename",packagename);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        }
+                        try{
+                            Thread.sleep(30);
+                        }catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+        }.start();
+
+    }
+
 }
