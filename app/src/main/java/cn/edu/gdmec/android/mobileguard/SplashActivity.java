4b1109776de8597440b60898f1b4e1b2ea154163
sun1969938509 <1969938509@qq.com>
2017-12-17 00:53:00 +0800

    点击程序锁没反应 AppLockActivity那里加上mFragments get（arg0)报错

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,5 +1,10 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,14 +17,24 @@ import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
 public class SplashActivity extends AppCompatActivity {
     private TextView mTvVersion;
     private String mVersion;
-
+    private static final int MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS=1101;
     @Override//注释
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       /* setContentView(R.layout.activity_splash);
+      setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
         mVersion= MyUtils.getVersion(getApplicationContext());
         mTvVersion=(TextView)findViewById(R.id.tv_spalsh_version);
         mTvVersion.setText("版本号:"+mVersion);
+        if(!hasPermission()){
+            startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+        }
+       /* VersionUpdateUtils.DownloadCallback downloadCallback=new VersionUpdateUtils.DownloadCallback() {
+            @Override
+            public void afterDownload(String filename) {
+                MyUtils.installApk(SplashActivity.this,filename);
+            }
+        };*/
         final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this,null, HomeActivity.class);
         new Thread(){
             @Override
@@ -30,7 +45,23 @@ public class SplashActivity extends AppCompatActivity {
             }
             }.start();
         }
-    }*/
+        private boolean hasPermission(){
+            AppOpsManager appOps=(AppOpsManager)getSystemService(Context.APP_OPS_SERVICE);
+            int mode=0;
+            if(Build.VERSION.SDK_INT>Build.VERSION_CODES.KITKAT){
+                mode=appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,android.os.Process.myUid(),getPackageName());
 
+            }
+            return mode==AppOpsManager.MODE_ALLOWED;
+        }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS){
+            if(!hasPermission()){
+                startActivityForResult(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS),MY_PERMISSIONS_REQUEST_PACKAGE_USAGE_STATS);
+            }
+        }
+       // super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Handler;
 
 import cn.edu.gdmec.android.mobileguard.App;
+import cn.edu.gdmec.android.mobileguard.m9advancedtools.service.AppLockService;
 
 /**
  * Created by Administrator on 2017/10/19.
@@ -15,5 +16,6 @@ public class BootCompleteReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
         ((App)(context.getApplicationContext())).correctSIM();
+        context.startService(new Intent(context, AppLockService.class));
     }
 }
